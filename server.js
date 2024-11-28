7e9975d9c1f7e4647b936356617f4a0898997639
CJ <166865964+CJ-S7Z777Z@users.noreply.github.com>
2024-11-28 09:17:55 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { Telegraf } = require('telegraf');
+const path = require('path');
+
+// Создаем бота
+const bot = new Telegraf('7895272383:AAG9RKIZ3Mw95bzOgBj3-TwvHc9ilAB2o2U');
+
+// Обработчик команды /start
+bot.start((ctx) => {
+  ctx.reply('Запуск мини-приложения', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { 
+            text: 'Открыть карту', 
+            web_app: { url: 'https://YOUR_DOMAIN_NAME/' } 
+          }
+        ]
+      ]
+    }
+  });
+});
+
+// Запуск бота
+bot.launch();
+
+// Создаем приложение Express
+const app = express();
+
+// Обслуживание статических файлов (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
